f86e06bbfe8ef13717272ea0fee0b564e6acdc3e
Ram Pandey <rampandey@Rams-MacBook-Pro.local>
2025-02-04 23:51:44 -0500

    added Sports Component

--- CourtLink/src/app/app.component.ts
+++ CourtLink/src/app/app.component.ts
@@ -1,11 +1,12 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { NavbarComponent } from './components/navbar/navbar.component';
+import { SportsComponent } from './components/sports/sports.component';
 
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet, NavbarComponent],
+  imports: [RouterOutlet, NavbarComponent, SportsComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
--- CourtLink/src/app/components/sports/sports.component.ts
+++ CourtLink/src/app/components/sports/sports.component.ts
@@ -1,11 +1,23 @@
 import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common'; // Import CommonModule for *ngFor
 
 @Component({
   selector: 'app-sports',
-  imports: [],
+  standalone: true,
+  imports: [CommonModule], // Include CommonModule for *ngFor support
   templateUrl: './sports.component.html',
-  styleUrl: './sports.component.css'
+  styleUrls: ['./sports.component.css']
 })
 export class SportsComponent {
+  sports = [
+    { name: 'Basketball', icon: 'https://img.icons8.com/?size=100&id=196&format=png&color=000000' },
+    { name: 'Soccer', icon: 'https://img.icons8.com/?size=100&id=9820&format=png&color=000000' },
+    { name: 'Tennis', icon: 'https://img.icons8.com/?size=100&id=48991&format=png&color=000000' },
+    { name: 'Badminton', icon: 'https://img.icons8.com/?size=100&id=24308&format=png&color=000000' },
+    { name: 'Cricket', icon: 'https://img.icons8.com/?size=100&id=4252&format=png&color=000000' },
+  ];
 
+  selectSport(sport: string): void {
+    console.log('Selected Sport:', sport);
+  }
 }
