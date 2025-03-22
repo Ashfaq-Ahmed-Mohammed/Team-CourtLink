692bdf99d9e1ebd81385178e2c381d9cc841507c
rohithcody <brohithkum@gmail.com>
2025-03-22 18:21:46 -0400

    Implemented create sport api fully

--- BackEnd/Sport/CreateSport.go
+++ BackEnd/Sport/CreateSport.go
@@ -8,18 +8,17 @@ import (
 
 func CreateSport(w http.ResponseWriter, r *http.Request) {
 	var s DataBase.Sport
-
 	err := json.NewDecoder(r.Body).Decode(&s)
+
 	if err != nil {
 		http.Error(w, "Invalid request body", http.StatusBadRequest)
 		return
 	}
-
 	var existingSport DataBase.Sport
 	result := DataBase.DB.Where("Sport_name = ?", s.Sport_name).First(&existingSport)
 
 	if result.RowsAffected > 0 {
-		http.Error(w, "The sport already exists", http.StatusBadRequest)
+		http.Error(w, "The sport record already exists", http.StatusBadRequest)
 		return
 	}
 
@@ -30,9 +29,8 @@ func CreateSport(w http.ResponseWriter, r *http.Request) {
 
 	w.Header().Set("Content-Type", "application/json")
 	w.WriteHeader(http.StatusCreated)
-
 	response := map[string]interface{}{
-		"message": "Sport record added successfully",
+		"message": "Sport record added successfully!!",
 		"sport":   s,
 	}
 	json.NewEncoder(w).Encode(response)
