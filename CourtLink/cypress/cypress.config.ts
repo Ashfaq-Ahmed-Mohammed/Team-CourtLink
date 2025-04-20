682593d7b61310051dda44b54a9ce26684943eaa
chetanramini <raminichetanreddy@gmail.com>
2025-04-19 20:26:56 -0400

    Unit testing for new fucntionality and cypress end to end testing sucessfully done

--- CourtLink/cypress/cypress.config.ts
+++ CourtLink/cypress/cypress.config.ts
@@ -2,7 +2,7 @@ import { defineConfig } from "cypress";
 
 export default defineConfig({
   e2e: {
-    baseUrl: "http://localhost:4200", // Update this if needed
+    baseUrl: "http://localhost:4200/?skipAuth=true", // Update this if needed
     supportFile: "cypress/support/e2e.ts",
     specPattern: "cypress/e2e/**/*.cy.ts",
   },
