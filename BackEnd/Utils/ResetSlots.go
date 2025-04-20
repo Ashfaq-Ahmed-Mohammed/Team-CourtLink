a0b8b3fa0d671d236ac2bbf22b9dbd3110775aae
rohithcody <brohithkum@gmail.com>
2025-04-20 01:35:49 -0400

    Added functionality to reset slots on court name

--- BackEnd/Utils/ResetSlots.go
+++ BackEnd/Utils/ResetSlots.go
@@ -6,25 +6,40 @@ import (
 )
 
 // ResetTimeSlotsForAvailableCourts resets all slots to 1 for courts that are available (Court_Status = 1)
-func ResetTimeSlotsForAvailableCourts() error {
-	var courtIDs []uint
+func ResetTimeSlotsForAvailableCourts(courtName string) error {
+	const (
+		AvailableStatus = 1
+		SlotAvailable   = 1
+	)
 
-	if err := DataBase.DB.
-		Model(&DataBase.Court{}).
-		Where("court_status = ?", 1).
-		Pluck("court_id", &courtIDs).Error; err != nil {
+	var courtIDs []uint
+	db := DataBase.DB.Model(&DataBase.Court{}).Where("court_status = ?", AvailableStatus)
+
+	// If a court name is provided, filter by that name
+	if courtName != "" {
+		db = db.Where("court_name = ?", courtName)
+	}
+
+	// Get the court IDs that match the condition(s)
+	if err := db.Pluck("court_id", &courtIDs).Error; err != nil {
 		return err
 	}
 
 	if len(courtIDs) == 0 {
-		log.Println("No available courts found to reset.")
+		if courtName != "" {
+			log.Printf("No available court found with name: %s\n", courtName)
+		} else {
+			log.Println("No available courts found to reset.")
+		}
 		return nil
 	}
 
 	slotReset := map[string]interface{}{
-		"slot_08_09": 1, "slot_09_10": 1, "slot_10_11": 1, "slot_11_12": 1,
-		"slot_12_13": 1, "slot_13_14": 1, "slot_14_15": 1, "slot_15_16": 1,
-		"slot_16_17": 1, "slot_17_18": 1,
+		"slot_08_09": SlotAvailable, "slot_09_10": SlotAvailable,
+		"slot_10_11": SlotAvailable, "slot_11_12": SlotAvailable,
+		"slot_12_13": SlotAvailable, "slot_13_14": SlotAvailable,
+		"slot_14_15": SlotAvailable, "slot_15_16": SlotAvailable,
+		"slot_16_17": SlotAvailable, "slot_17_18": SlotAvailable,
 	}
 
 	if err := DataBase.DB.
@@ -34,6 +49,11 @@ func ResetTimeSlotsForAvailableCourts() error {
 		return err
 	}
 
-	log.Printf("Reset time slots for %d available court(s).\n", len(courtIDs))
+	if courtName != "" {
+		log.Printf("Reset time slots for court: %s\n", courtName)
+	} else {
+		log.Printf("Reset time slots for %d available court(s).\n", len(courtIDs))
+	}
+
 	return nil
 }
